ecf0928e88a0b669f3ac087a730a10ab9bb8ed16
dnacu <winteri1276@gmail.com>
2020-08-11 20:38:35 +0900

    chore: styled-components 설치

--- client/src/pages/MainDashboard/MainDashboard.tsx
+++ client/src/pages/MainDashboard/MainDashboard.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
 
 type Props = {}
 
+const Dashboard = styled.main`
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  overflow-y: auto;
+`
+
 export const MainDashboard = (props: Props) => {
-  return <div>Halo</div>
+  return <Dashboard className="dashboard"></Dashboard>
 }

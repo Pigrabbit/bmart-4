7d1b4c83807eb2d2485826f14ba017968f0ba125
dnacu <winteri1276@gmail.com>
2020-08-11 20:18:21 +0900

    feat: react-router-dom을 이용하여 maindashboard 페이지 라우팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { MainDashboard } from './pages/MainDashboard'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <MainDashboard />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App

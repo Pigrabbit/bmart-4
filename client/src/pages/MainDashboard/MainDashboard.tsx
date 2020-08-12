564e952f6fc43b66b334589904321c4ce348e67f
dnacu <winteri1276@gmail.com>
2020-08-12 22:50:27 +0900

    chore: mockup데이터를 활용한 메인 뷰 구성

--- client/src/pages/MainDashboard/MainDashboard.tsx
+++ client/src/pages/MainDashboard/MainDashboard.tsx
@@ -4,64 +4,7 @@ import styled from 'styled-components'
 import { Carousel } from '../../components/Carousel'
 import { VerticalList } from '../../components/VerticalList'
 import { HorizontalList } from '../../components/HorizontalList'
-import { ProductCardType } from '../../types/productCard'
-
-const productList: ProductCardType[] = [
-  {
-    name: '비요뜨 초코링 143g',
-    price: 1390,
-    thumbnail:
-      'https://thumbnail7.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/2019/08/09/15/2/267a0947-5985-43d7-8e65-5c0520d892e0.jpg',
-  },
-  {
-    name: '홈런볼 초코 46g',
-    price: 1290,
-    thumbnail:
-      'https://thumbnail7.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/2019/08/09/15/2/267a0947-5985-43d7-8e65-5c0520d892e0.jpg',
-  },
-  {
-    name: '서울우유 100ml',
-    price: 3090,
-    thumbnail:
-      'https://thumbnail7.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/2019/08/09/15/2/267a0947-5985-43d7-8e65-5c0520d892e0.jpg',
-  },
-  {
-    name: '비요뜨 초코링 143g',
-    price: 1390,
-    thumbnail:
-      'https://thumbnail7.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/2019/08/09/15/2/267a0947-5985-43d7-8e65-5c0520d892e0.jpg',
-  },
-  {
-    name: '홈런볼 초코 46g',
-    price: 1290,
-    thumbnail:
-      'https://thumbnail7.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/2019/08/09/15/2/267a0947-5985-43d7-8e65-5c0520d892e0.jpg',
-  },
-  {
-    name: '비요뜨 초코링 143g',
-    price: 1390,
-    thumbnail:
-      'https://thumbnail7.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/2019/08/09/15/2/267a0947-5985-43d7-8e65-5c0520d892e0.jpg',
-  },
-  {
-    name: '홈런볼 초코 46g',
-    price: 1290,
-    thumbnail:
-      'https://thumbnail7.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/2019/08/09/15/2/267a0947-5985-43d7-8e65-5c0520d892e0.jpg',
-  },
-  {
-    name: '서울우유 100ml',
-    price: 3090,
-    thumbnail:
-      'https://thumbnail7.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/2019/08/09/15/2/267a0947-5985-43d7-8e65-5c0520d892e0.jpg',
-  },
-  {
-    name: '비요뜨 초코링 143g',
-    price: 1390,
-    thumbnail:
-      'https://thumbnail7.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/2019/08/09/15/2/267a0947-5985-43d7-8e65-5c0520d892e0.jpg',
-  },
-]
+import { productList, bigBannerList, smallBannerList } from '../../utils/mockData'
 
 type Props = {}
 
@@ -77,14 +20,15 @@ const Container = styled.main`
 export const MainDashboard = (props: Props) => {
   return (
     <Container className="dashboard">
-      <VerticalList title="성현님을 위해 준비한 상품" productList={productList} />
-      <Carousel width={window.innerWidth} />
-      <HorizontalList title="동혁님을 위해 준비한 상품" productList={productList} />
+      <Carousel bannerList={bigBannerList} />
       <HorizontalList
         title="성현님을 위해 준비한 상품"
         productList={productList}
         double={true}
       ></HorizontalList>
+      <Carousel bannerList={smallBannerList} />
+      <HorizontalList title="동혁님을 위해 준비한 상품" productList={productList} />
+      <VerticalList title="성현님을 위해 준비한 상품" productList={productList} />
     </Container>
   )
 }

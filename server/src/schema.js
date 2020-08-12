8366a9799d6f0a8a9cad3ee0b14173c93b31f580
Pigrabbit <donghyuk1003@gmail.com>
2020-08-12 23:58:31 +0900

    feat: ProdcutType 및 query type 추가

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,7 +1,40 @@
-const { GraphQLSchema } = require('graphql')
+const { GraphQLSchema, GraphQLObjectType, GraphQLNonNull, GraphQLID, GraphQLString, GraphQLInt, GraphQLList } = require('graphql')
+
+const ProductType = new GraphQLObjectType({
+    name: 'Product',
+    description: 'This represents product in store',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        coupang_product_id: { type: GraphQLNonNull(GraphQLInt) },
+        category: { type: GraphQLNonNull(GraphQLString) },
+        price: { type: GraphQLNonNull(GraphQLInt) },
+        base_price: { type: GraphQLInt },
+        discount_rate: { type: GraphQLInt },
+        thumbnail_src: { type: GraphQLNonNull(GraphQLString) },
+        created_at: { type: GraphQLNonNull(GraphQLString) },
+        stock_count: { type: GraphQLNonNull(GraphQLInt) },
+        sold_count: { type: GraphQLNonNull(GraphQLInt) },
+        description: { type: GraphQLString },
+    })
+})
+
+const RootQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Root Query',
+    fields: () => ({
+        productList: {
+            type: new GraphQLList(ProductType),
+            description: 'List of products',
+            resolve: async () => {
+                // get Prodcut from db
+            }
+        }
+    })
+})
 
 const schema = new GraphQLSchema({
-
+    query: RootQueryType
 })
 
 module.exports = schema

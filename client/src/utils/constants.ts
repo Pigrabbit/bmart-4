c7af4d9b2fe29e1c9d009221e51a53290964e8c4
dnacu <winteri1276@gmail.com>
2020-08-13 19:30:06 +0900

    chore: constants에 카테고리 정의

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,4 +1,17 @@
-export const CS_PHONE:string = "1600-0025"
-export const CS_EMAIL:string = "bmart_together@woowahan.com"
+export const CS_PHONE: string = '1600-0025'
+export const CS_EMAIL: string = 'bmart_together@woowahan.com'
 export const SLIDER_INTERVAL_TIME = 2000
 export const GRAPHQL_URI = 'http://localhost:4000/graphql'
+export const CATEGORIES = [
+  '가루/조미료/오일',
+  '유제품/아이스크림',
+  '생수/음료',
+  '축산/계란',
+  '수산물/건어물',
+  '냉장/냉동/간편요리',
+  '과일',
+  '반찬/간편식/대용식',
+  '장/소스/드레싱/식초',
+  '과자/초콜릿/시리얼',
+  '채소',
+]

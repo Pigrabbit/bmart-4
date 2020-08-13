a5bc0f359d0663f14e6e1493e494b0be88a1955b
Sunghyun Cho <mail@chosunghyun.com>
2020-08-13 15:24:15 +0900

    style: B마트 헤더에 스타일 컴포넌트 적용

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,12 +1,38 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const StyledContainer = styled.footer``
+const StyledContainer = styled.header`
+  display: flex;
+  padding: 20px;
+`
+
+const StyledImage = styled.img`
+  width: 40px;
+  height: 40px;
+`
+
+const StyledTitle = styled.h1`
+  margin: auto auto auto 0;
+`
+
+const StyledOptions = styled.div`
+  display: flex;
+`
+
+const StyledButton = styled.button`
+  padding: 10px;
+  margin: auto 0 auto 10px;
+`
 
 export const Header = () => {
   return (
     <StyledContainer className="header" data-testid="header">
-      <h1>B 마트</h1>
+      <StyledImage src={`${process.env.PUBLIC_URL}/images/bmart-logo.png`} alt="" />
+      <StyledTitle>마트</StyledTitle>
+      <StyledOptions>
+        <StyledButton>검색</StyledButton>
+        <StyledButton>옵션</StyledButton>
+      </StyledOptions>
     </StyledContainer>
   )
 }

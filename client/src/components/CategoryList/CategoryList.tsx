bfafb02031ed6035c68e6e1fb2a3e81267120719
Sunghyun Cho <mail@chosunghyun.com>
2020-08-14 13:47:33 +0900

    refactor: map을 사용한 카테고리 생성
    - 카테고리 리스트를 받아서 카테고리를 생성함

--- client/src/components/CategoryList/CategoryList.tsx
+++ client/src/components/CategoryList/CategoryList.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { CATEGORY_LIST } from '../../utils/constants'
 
 const StyledCategoryList = styled.div`
   display: grid;
@@ -9,64 +10,27 @@ const StyledCategoryList = styled.div`
   padding: 10px 0px;
 `
 
-const StyledCategory = styled.img`
+const StyledCategory = styled.a``
+
+const StyledCategoryIcon = styled.img`
   max-width: 100%;
   margin: 5px 0px;
 `
 
 export const CategoryList = () => {
+  const categoryList = CATEGORY_LIST
   return (
     <StyledCategoryList className="category-list">
-      <StyledCategory
-        className="category-list-salad"
-        src={`${process.env.PUBLIC_URL}/images/icons/lossy-salad.png`}
-        alt=""
-      />
-      <StyledCategory
-        className="category-list-egg"
-        src={`${process.env.PUBLIC_URL}/images/icons/lossy-egg.png`}
-        alt=""
-      />
-      <StyledCategory
-        className="category-list-mealkit"
-        src={`${process.env.PUBLIC_URL}/images/icons/lossy-mealkit.png`}
-        alt=""
-      />
-      <StyledCategory
-        className="category-list-milk"
-        src={`${process.env.PUBLIC_URL}/images/icons/lossy-milk.png`}
-        alt=""
-      />
-      <StyledCategory
-        className="category-list-bread"
-        src={`${process.env.PUBLIC_URL}/images/icons/lossy-bread.png`}
-        alt=""
-      />
-      <StyledCategory
-        className="category-list-yasik"
-        src={`${process.env.PUBLIC_URL}/images/icons/lossy-yasik.png`}
-        alt=""
-      />
-      <StyledCategory
-        className="category-list-choco"
-        src={`${process.env.PUBLIC_URL}/images/icons/lossy-choco.png`}
-        alt=""
-      />
-      <StyledCategory
-        className="category-list-ice"
-        src={`${process.env.PUBLIC_URL}/images/icons/lossy-ice.png`}
-        alt=""
-      />
-      <StyledCategory
-        className="category-list-hair"
-        src={`${process.env.PUBLIC_URL}/images/icons/lossy-hair.png`}
-        alt=""
-      />
-      <StyledCategory
-        className="category-list-more"
-        src={`${process.env.PUBLIC_URL}/images/icons/lossy-more.png`}
-        alt=""
-      />
+      {categoryList.map((category, idx) => (
+        <StyledCategory key={idx} href={`/category/${category}`} className="category-list-item">
+          <StyledCategoryIcon
+            src={`${process.env.PUBLIC_URL}/images/icons/${category}.png`}
+            alt={`${category}-icon`}
+            className="category-icon"
+            id={`${category}-icon`}
+          />
+        </StyledCategory>
+      ))}
     </StyledCategoryList>
   )
 }

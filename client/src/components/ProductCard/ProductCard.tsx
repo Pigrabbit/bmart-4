0cad3392e58d15bcbbe3fe55945ae5c9bb0a1bbc
dnacu <winteri1276@gmail.com>
2020-08-16 15:27:22 +0900

    chore: define StyledLink component
    - react-router-dom의 Link가 a태그여서 하위 태그들의 css가 망가짐 - styledComponent로 wrapping하여 스타일 속성을 선언함으로써 방지

--- client/src/components/ProductCard/ProductCard.tsx
+++ client/src/components/ProductCard/ProductCard.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { parseToLocalMoneyString } from '../../utils/parser'
 import { ProductCardType } from '../../types/productCard'
+import { StyledLink } from '../../styles/StyledLink'
 
 type Props = {
   product: ProductCardType
@@ -32,17 +33,17 @@ export const ProductCard = (props: Props) => {
   const { product, width = '50%', style } = props
   const { id, price, name, thumbnailSrc } = product
 
-  const cardClickHandler = () => (window.location.href = `product/${id}`)
-
   return (
-    <StyledContainer width={width} style={style} onClick={cardClickHandler}>
-      <StyledThumbnail>
-        <img className="thumbnail" src={`http://${thumbnailSrc}`} alt="" />
-      </StyledThumbnail>
-      <StyledContent>
-        <div className="product-name">{name}</div>
-        <div className="price">{parseToLocalMoneyString(price)}원</div>
-      </StyledContent>
+    <StyledContainer width={width} style={style}>
+      <StyledLink to={`product/${id}`}>
+        <StyledThumbnail>
+          <img className="thumbnail" src={`http://${thumbnailSrc}`} alt="" />
+        </StyledThumbnail>
+        <StyledContent>
+          <div className="product-name">{name}</div>
+          <div className="price">{parseToLocalMoneyString(price)}원</div>
+        </StyledContent>
+      </StyledLink>
     </StyledContainer>
   )
 }

499458d3c8737858308cdde13d0fd88698e78ae9
Pigrabbit <donghyuk1003@gmail.com>
2020-08-19 14:28:39 +0900

    feat: CarouselBasic 컴포넌트 indicator 구현
    - IntersectionObserve API를 활용하여 구현

--- client/src/components/CarouselBasic/CarouselBasic.tsx
+++ client/src/components/CarouselBasic/CarouselBasic.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { BannerType } from '../../types/banner'
 import styled from 'styled-components'
 import { StyledWrapper } from '../../styles/StyledWrapper'
@@ -26,10 +26,18 @@ const StyledCarousel = styled.div`
     justify-content: space-around;
   }
 
-  .carousel-indicator {
+  .carousel-indicator-invisible {
     height: 16px;
     width: 16px;
     background-color: transparent;
+    border: 1px solid #bbb;
+    border-radius: 50%;
+  }
+
+  .carousel-indicator-visible {
+    height: 16px;
+    width: 16px;
+    background-color: #bbb;
     border: 1px solid #bbb;
     border-radius: 50%;
   }
@@ -60,26 +68,49 @@ const StyledSlider = styled.div`
 
 export const CarouselBasic = (props: Props) => {
   const { bannerList } = props
-  const [currentBannerIdx, setCurrentBannerIdx] = useState(0)
+  const silderRef = useRef<HTMLDivElement>(null)
+  const bannerRefList = useRef<Array<HTMLDivElement | null>>([])
+  const [bannerIndex, setBannerIndex] = useState(0)
 
-  const scrollHandler = () => {
-    console.log('hi')
-  }
+  useEffect(() => {
+    const bannerObserveHandler = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && entry.intersectionRatio >= 0.5) {
+          setBannerIndex(bannerRefList.current.indexOf(entry.target as HTMLDivElement))
+        }
+      })
+    }
+    const observer = new IntersectionObserver(bannerObserveHandler, {
+      root: silderRef.current,
+      threshold: 0.5,
+    })
+    bannerRefList.current.forEach((banner) => {
+      observer.observe(banner!)
+    })
+  }, [])
 
   return (
     <StyledWrapper>
       <StyledCarousel className="carousel">
-        <StyledContainer className="carousel-slide-container" onScroll={scrollHandler}>
+        <StyledContainer className="carousel-slide-container" ref={silderRef}>
           {bannerList.map((item, idx) => (
-            <StyledSlider key={idx} className="carousel-slide">
+            <StyledSlider
+              key={idx}
+              className="carousel-slide"
+              ref={(el) => {
+                bannerRefList.current[idx] = el
+              }}
+            >
               <img src={item.src} alt="" />
             </StyledSlider>
           ))}
         </StyledContainer>
         <div className="carousel-indicator-list">
-          {bannerList.map((item, idx) => (
-            <div key={idx} className="carousel-indicator"></div>
-          ))}
+          {bannerList.map((item, idx) => {
+            return idx === bannerIndex ?
+              <div key={idx} className="carousel-indicator-visible"></div> :
+              <div key={idx} className="carousel-indicator-invisible"></div>
+          })}
         </div>
       </StyledCarousel>
     </StyledWrapper>

d33795450cc19b87de0f862eaffae6f67dca275a
Pigrabbit <donghyuk1003@gmail.com>
2020-08-19 23:29:38 +0900

    test: ProductCard UI 테스트 수정
    - ProductCard 컴포넌트에서 router의 to로 object를 넘겨줌에 따라 test code 수정

--- client/src/components/ProductCard/ProductCard.spec.tsx
+++ client/src/components/ProductCard/ProductCard.spec.tsx
@@ -12,6 +12,7 @@ const testProduct: ProductCardType = {
   price: 1000000,
   thumbnailSrc:
     'thumbnail7.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/2019/08/09/15/2/267a0947-5985-43d7-8e65-5c0520d892e0.jpg',
+  coupangProductId: '111111111'
 }
 
 const testProps: Props = {
@@ -30,7 +31,7 @@ describe('<ProductCard />', () => {
   })
 
   it('render: 썸네일, 링크, 상품이름, 가격이 올바르게 노출되는가', () => {
-    expect(wrapper.find(Link).props().to).toBe(`/product/${testProps.product.id}`)
+    expect(wrapper.find(Link).props().to["pathname"]).toBe(`/product/${testProps.product.id}`)
 
     const productNameElm = wrapper.find('.product-name')
     expect(productNameElm.contains('테스트 상품명')).toBe(true)

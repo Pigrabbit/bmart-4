c31a7db08483228d92b4d0662914ad8a7281092e
Pigrabbit <donghyuk1003@gmail.com>
2020-08-24 10:35:30 +0900

    feat: 검색 결과페이지 정렬 기능 구현

--- client/src/pages/SearchResultDashboard/SearchResultDashboard.tsx
+++ client/src/pages/SearchResultDashboard/SearchResultDashboard.tsx
@@ -1,25 +1,87 @@
-import React from 'react'
+import React, { useState, useReducer } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 import { ProductCardType } from '../../types/productCard'
 import { Dashboard } from '../../components/Dashboard'
 import { VerticalList } from '../../components/VerticalList'
+import { Sorter } from '../../components/Sorter'
 
 type Props = {} & RouteComponentProps
 
-type State = {
+type RouteState = {
   searchResultList: ProductCardType[] 
 }
 
-export const SearchResultDashboard = (props: Props) => {
-  const { location } = props
-  if (!location.state) return null
-  const state = location.state as State
+type State = {
+  resultList: ProductCardType[]
+  sorter: number
+}
 
-  const { searchResultList } = state
-  
+type Action = {
+  type: string
+  payload: any
+}
+
+const searchResultReducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case 'sorterChange': {
+      console.log(action.payload.sorter)
+      if (action.payload.sorter === 0) {
+        return {
+          resultList: [...state.resultList]
+            .sort((a, b) => {
+              if (a.id > b.id) return 1
+              else return -1
+            }),
+          sorter: action.payload.sorter
+        }
+      } else if (action.payload.sorter === 1) {
+        return {
+          resultList: [...state.resultList]
+            .sort((a, b) => {
+              if (a.price > b.price) return 1
+              else return -1
+            }),
+          sorter: action.payload.sorter
+        }
+      } else if (action.payload.sorter === 2) {
+        return {
+          resultList: [...state.resultList]
+            .sort((a, b) => {
+              if (a.price < b.price) return 1
+              else return -1
+            }),
+          sorter: action.payload.sorter
+        }
+      }
+    }
+  }
+  return state
+}
+
+export const SearchResultDashboard = (props: Props) => { 
+  const { location } = props
+  const routeState = location.state as RouteState
+  const { searchResultList } = routeState
+
+  const initialState = {
+    resultList: searchResultList,
+    sorter: 0
+  }
+    
+  const [state, dispatch] = useReducer(searchResultReducer, initialState)
+
+   // useState를 활용해서 sorter 상태 관리
+   const [sorter, setSorter] = useState(0)
+   const sorterChangeHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+      dispatch({ type: 'sorterChange', payload: { sorter: parseInt(event.target.value) }})
+   }
+
   return (
     <Dashboard title="" header={false}>
-        <VerticalList title="검색 결과" productList={searchResultList} />
+      <>
+        <Sorter sorterChangeHandler={sorterChangeHandler}/>
+        <VerticalList title="검색 결과" productList={state.resultList} />
+      </>
     </Dashboard>
   )
 }

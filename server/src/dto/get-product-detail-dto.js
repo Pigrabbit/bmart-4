2146dc563487274ada6ef7d4aebd972bb0e5fdff
Sunghyun Cho <mail@chosunghyun.com>
2020-08-25 14:58:48 +0900

    chore: use https instead of http

--- server/src/dto/get-product-detail-dto.js
+++ server/src/dto/get-product-detail-dto.js
@@ -1,12 +1,8 @@
 class GetProductDetailDTO {
-  constructor({
-    id,
-    coupang_product_id,
-    img_src,
-  }) {
+  constructor({ id, coupang_product_id, img_src }) {
     this.id = id
     this.coupangProductId = coupang_product_id
-    this.src = `http://${img_src}`
+    this.src = `https://${img_src}`
   }
 }
 

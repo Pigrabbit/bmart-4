9c43a95d8de0f25984b11d083ed42dcd63869459
Pigrabbit <donghyuk1003@gmail.com>
2020-08-27 11:10:26 +0900

    style: SearchDashboard 스타일 개선
    - 검색어 추천기능 구현을 위한 UI 개선

--- client/src/pages/SearchDashboard/SearchDashboard.tsx
+++ client/src/pages/SearchDashboard/SearchDashboard.tsx
@@ -1,29 +1,47 @@
-import React, { useState, useRef, FormEvent, useReducer } from 'react'
+import React, { useState, useRef, FormEvent, useReducer, useEffect } from 'react'
 import { Redirect } from 'react-router-dom'
 import { Dashboard } from '../../components/Dashboard'
 import styled from 'styled-components'
 import { SEARCH_URI, SPECIAL_CHAR_REGEX, MAX_SEARCH_QUERY_LENGTH } from '../../utils/constants'
 import { ProductCardType } from '../../types/productCard'
+import { COLORS, STYLES } from '../../utils/styleConstants'
 
 type Props = {}
 
-const StyledContainer = styled.section`
-  margin-top: 30px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  .search-form {
-    width: 80%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+const StyledContainer = styled.div`
+  padding: ${STYLES.padding};
+  display: grid;
+  grid-template-rows: repeat(auto-fill, minmax(40px, 1fr));
+  justify-items: center;
+
+  .search-alert {
+    color: ${COLORS.red};
   }
+
+  .search-suggestion {
+    height: 30px;
+    width: 90%;
+    padding: 8px 12px;
+    background-color: #fff;
+    justify-self: start;
+  }
+`
+
+const StyledForm = styled.form`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${COLORS.lightBlue};
 `
 
 const StyledInput = styled.input`
   width: 90%;
   border: none;
-  height: 24px;
+  height: 40px;
+  border-radius: 8px;
+  border: 1px solid #ddd;
+  padding: 0 12px;
 `
 
 const StyledSubmitBtn = styled.button`
@@ -78,6 +96,11 @@ const initialState = {
 
 export const SearchDashboard = (props: Props) => {
   const [state, dispatch] = useReducer(searchReducer, initialState)
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [])
 
   const submitHandler = async (e: FormEvent) => {
     e.preventDefault()
@@ -102,12 +125,15 @@ export const SearchDashboard = (props: Props) => {
         />
       ) : (
         <StyledContainer className="search-dashboard">
-          <form className="search-form" onSubmit={submitHandler}>
+          <StyledForm className="search-form" onSubmit={submitHandler}>
             <StyledInput
               className="search-input"
               type="text"
+              ref={inputRef}
               value={state.query}
               name="query"
+              placeholder="B마트 상품을 검색해보세요!"
+              autoComplete="off"
               onChange={(e) => dispatch({ type: 'input', payload: { query: e.target.value } })}
             />
             <StyledSubmitBtn
@@ -117,8 +143,15 @@ export const SearchDashboard = (props: Props) => {
             >
               <img src={`${process.env.PUBLIC_URL}/images/navbar-icon/search.svg`} />
             </StyledSubmitBtn>
-          </form>
-          {state.isQueryLengthOverLimit ? <p>검색어는 30자 이하로 입력해주세요</p> : ''}
+          </StyledForm>
+          <div className="search-suggestion">{state.query}</div>
+          <div className="search-suggestion">{state.query + " 1개"}</div>
+          <div className="search-suggestion">{state.query + " 어린이"}</div>
+          {state.isQueryLengthOverLimit ? (
+            <p className="search-alert">검색어는 30자 이하로 입력해주세요</p>
+          ) : (
+            ''
+          )}
         </StyledContainer>
       )}
     </Dashboard>
